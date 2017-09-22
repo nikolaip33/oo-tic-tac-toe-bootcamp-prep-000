25c0207b83f82059ee13ff6f761cb5718337c653
Nikolai Pawlowski <nikolaip@gmail.com>
2017-09-22 08:47:54 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -33,4 +33,8 @@ class TicTacToe
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
+
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(@board, index)
+  end
 end
